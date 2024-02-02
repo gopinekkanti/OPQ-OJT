d7d86dff40ee4c4223780565dc51031aed93e671
gopinekkanti <107097631+gopinekkanti@users.noreply.github.com>
2024-02-02 15:43:33 +0530

    Create pub-rt.tf

---
+++ modules/vpc/pub-rt.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "ojt-pub-RT" {
+  depends_on = [ aws_vpc.OJT-VPC ]
+  vpc_id = aws_vpc.OJT-VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.OJT-IGW.id
+  }
+  tags = {
+    Name = "ojt-pub-RT"
+  }
+}
+
+resource "aws_route_table_association" "ojt-pub-RT-association" {
+    subnet_id = aws_subnet.ojt-public-subnet-pub.id
+    route_table_id = aws_route_table.dev-pub-RT.id
+}
